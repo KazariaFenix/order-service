d88a76728403bd04fde0d1e777a99be3c393e0cd
DavtyanSV <davtyan_sv@mail.ru>
2023-12-22 18:09:27 +0300

    create OrderGoods dto and entity

---
+++ src/main/java/aston/red/orderservice/entity/OrderGoodsEntity.java
@@ -0,0 +1,33 @@
+package aston.red.orderservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "OrderGoods")
+@Data
+public class OrderGoodsEntity {
+
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "good_name")
+  private String good_name;
+
+  @Column(name = "quantity")
+  private Integer quantity;
+
+  @ManyToOne
+  @JoinColumn(name = "orderId")
+  private Order order;
+
+}
