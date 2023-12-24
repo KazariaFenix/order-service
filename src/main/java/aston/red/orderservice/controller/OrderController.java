f019674e1285dcb1cc86bd3ccf11fb02aaaec033
Vladimir <KazariaF@yandex.ru>
2023-12-24 13:04:27 +0300

    add: controller, service and repository class

--- src/main/java/aston/red/orderservice/controller/OrderController.java
+++ src/main/java/aston/red/orderservice/controller/OrderController.java
@@ -1,13 +1,18 @@
 package aston.red.orderservice.controller;
 
+import aston.red.orderservice.service.OrdersGoodsService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class OrderController {
+
+    private OrdersGoodsService service;
 
     @GetMapping
     public void payedOrder(Long orderId) {
-
+        
     }
 }
--- src/main/java/aston/red/orderservice/service/impl/OrdersGoodsService.java
+++
@@ -1,8 +0,0 @@
-package aston.red.orderservice.service.impl;
-
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class OrdersGoodsService {
-}
