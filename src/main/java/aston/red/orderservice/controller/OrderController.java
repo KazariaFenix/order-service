8d12854a23916cf25abd39939fc0b7b72dc33b39
Elena <94721785+HelenBarskaya@users.noreply.github.com>
2023-12-26 19:04:16 +0400

    OrderController (#6)

--- src/main/java/aston/red/orderservice/controller/OrderController.java
+++ src/main/java/aston/red/orderservice/controller/OrderController.java
@@ -1,21 +1,25 @@
 package aston.red.orderservice.controller;
 
+import aston.red.orderservice.dto.OrderGoodsDto;
+import aston.red.orderservice.dto.OrderPreparedToPayDto;
 import aston.red.orderservice.service.OrderService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/order")
 @AllArgsConstructor
 public class OrderController {
 
-    private final OrderService service;
+    private final OrderService orderService;
 
     @PostMapping("/{orderId}")
-    public void payedOrder(@RequestParam Long orderId) {
-        service.postDeliveryOrder(orderId);
+    public void payedOrder(@PathVariable Long orderId) {
+        orderService.postDeliveryOrder(orderId);
+    }
+
+    @PostMapping(produces = "application/json", consumes = "application/json")
+    public OrderPreparedToPayDto processOrder(@RequestBody OrderGoodsDto orderGoodsDto) {
+        return orderService.processOrder(orderGoodsDto);
     }
 }
--- src/main/java/aston/red/orderservice/repository/OrdersGoodsRepository.java
+++ src/main/java/aston/red/orderservice/repository/OrdersGoodsRepository.java
@@ -2,6 +2,8 @@ package aston.red.orderservice.repository;
 
 import aston.red.orderservice.entity.OrderGoodsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrdersGoodsRepository extends JpaRepository<OrderGoodsEntity, Long> {
 }
