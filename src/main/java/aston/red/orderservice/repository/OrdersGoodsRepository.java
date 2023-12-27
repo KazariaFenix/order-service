7faf9f98624724df3894e4bcc60a979095cf8521
Vladimir <KazariaF@yandex.ru>
2023-12-27 19:17:25 +0300

    fix: conflict branch merge

--- src/main/java/aston/red/orderservice/repository/OrdersGoodsRepository.java
+++ src/main/java/aston/red/orderservice/repository/OrdersGoodsRepository.java
@@ -3,5 +3,9 @@ package aston.red.orderservice.repository;
 import aston.red.orderservice.entity.OrderGoodsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrdersGoodsRepository extends JpaRepository<OrderGoodsEntity, Long> {
+
+    List<OrderGoodsEntity> findByOrder
 }
