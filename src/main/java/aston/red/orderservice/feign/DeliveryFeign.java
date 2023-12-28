e8a6605175ca3ae8fd37130dab162aa0084b5c40
Vladimir <KazariaF@yandex.ru>
2023-12-28 20:03:25 +0300

    fix:

--- src/main/java/aston/red/orderservice/feign/DeliveryFeign.java
+++ src/main/java/aston/red/orderservice/feign/DeliveryFeign.java
@@ -2,12 +2,12 @@ package aston.red.orderservice.feign;
 
 import aston.red.orderservice.dto.OrderDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(value = "deliveryOrder", url = "http://localhost:8097/api/v1/deliveries")
 public interface DeliveryFeign {
 
-    @PatchMapping("/order")
+    @PutMapping("/order")
     void postOrderDto(@RequestBody OrderDto dto);
 }
